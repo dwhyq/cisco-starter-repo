d30d589da1bd4eaa85b9e08b7a980679366e504d
dwhyq <dwhyq@outlook.com>
2023-09-07 11:25:42 +0100

    install dependency and built the latency component

---
+++ sextant/src/latency/Latency.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Latency.css'
+
+const Latency = () => {
+  return (
+    <div className='latency'>
+      <h2>Latency</h2>
+    </div>
+  )
+}
+
+export default Latency
