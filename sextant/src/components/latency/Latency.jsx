72de2247645e6f6b25c983f838326a17b7152a0f
dwhyq <dwhyq@outlook.com>
2023-09-12 11:27:26 +0100

    get latency

--- sextant/src/components/latency/Latency.jsx
+++ sextant/src/components/latency/Latency.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import './Latency.css'
+import React, { Component } from 'react';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+const client = new W3CWebSocket('ws://localhost:55455');
 
-const Latency = () => {
-  return (
-    <div className='latency'>
-      <h2>Latency</h2>
-    </div>
-  )
+class Latency extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            latency: null
+        };
+    }
+
+    componentDidMount() {
+        client.onmessage = (message) => {
+            this.setState({
+                latency: new Date().getTime() - message.data
+            })
+        };
+    }
+
+    render() {
+        return (
+          <div>
+            <h2>Latency</h2>
+            <span className="PylonConnector">
+                {this.state.latency}
+            </span>
+          </div>
+        );
+    }
 }
 
-export default Latency
+export default Latency;
